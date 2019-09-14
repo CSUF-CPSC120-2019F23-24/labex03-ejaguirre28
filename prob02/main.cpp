67ca21b5c3b966f4fa764ffc2d45f1920d253b0a
ejaguirre <ejaguirre@cs104u-3c.acad.fullerton.edu>
2019-09-14 12:59:11 -0700

    changed spacing and indents in code

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -6,15 +6,15 @@ int main()
 {
   // one gallon of paint covers 340 square feet
   int coverage = 340;
-   int height = 6;
-    int length = 100;
+  int height = 6;
+  int length = 100;
 
   // calculate how many gallons of paint it will take to cover
   // the area with 2 coats of paint
-  double area=height*length*2;
-    double gallons = area / coverage;
+  double area=height*length*2 ;
+  double gallons = area / coverage ;
 
-  std::cout << "It will take " << gallons << " gallons of paint " << '\n';
+  std::cout << " It will take " << gallons << " gallons of paint " << '\n' ;
 
-  return 0;
+  return 0 ;
 }
